5a01015c6534486ccb144a9b2312eda8c53b9e9b
root <jsw800@gmail.com>
2018-10-17 09:39:47 -0600

    bar really fixed, orange rofi theme edited

--- .i3scripts/fix_volume.py
+++ .i3scripts/fix_volume.py
@@ -2,7 +2,7 @@
 
 import sys
 
-x = [line for line in sys.stdin.read().split('\n') if line != '']
+x = [line for line in sys.stdin.read().split('\n') if line != '' and '#' not in line]
 
 x[0] = x[0][:-18]
 x[1] = x[1][:-4]
